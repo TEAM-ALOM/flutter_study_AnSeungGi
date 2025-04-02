7e7f379b0b5a0e045ca5726ea9b9e6678acdf28d
ooooo333i <antmdrl0531@gmail.com>
2025-04-03 01:37:46 +0900

    fix_gitignore

--- ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/anseung-gi/folder/flutter_app/ios/Runner/GeneratedPluginRegistrant.m \
-  /Users/anseung-gi/folder/flutter_app/ios/Runner/GeneratedPluginRegistrant.h \
-  /Users/anseung-gi/folder/flutter_app/build/ios/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap
--- ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/anseung-gi/folder/flutter_app/ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
