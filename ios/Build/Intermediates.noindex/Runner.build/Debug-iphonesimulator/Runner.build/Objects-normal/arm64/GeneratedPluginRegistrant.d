294231729639d64c811821d83dc5e842159d4337
ooooo333i <antmdrl0531@gmail.com>
2025-04-03 00:58:57 +0900

    Commit1

---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/anseung-gi/folder/flutter_app/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/anseung-gi/folder/flutter_app/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/anseung-gi/folder/flutter_app/build/ios/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap
---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/anseung-gi/folder/flutter_app/ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
