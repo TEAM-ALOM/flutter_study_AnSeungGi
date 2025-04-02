ae8cac6487eea4d52bb70dbc240429e032673d02
ooooo333i <antmdrl0531@gmail.com>
2025-04-03 01:11:37 +0900

    import http

---
+++ lib/quiz.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Quiz {
+  String question = '';
+  List<String> answers = [];
+  String coanswer = '';
+  Quiz({required this.answers, required this.question, required this.coanswer});
+}
+
+Future<Quiz> fetching() async {
+  final uri = Uri.parse('https://opentdb.com/api.php?amount=1&type=multiple');
+  final response = await http.get(uri);
+
+  if (response.statusCode != 200) {
+    throw Exception('Failed to load quiz data');
+  }
+
+  final data = json.decode(response.body);
+  //(data);
+  final result = data['results'][0];
+
+  String question = result['question'];
+  List<String> answers = List<String>.from(result['incorrect_answers']);
+  String coanswer = result['correct_answer'];
+  List<String> anlist = [...answers,coanswer];
+  anlist.shuffle();
+  
+  return Quiz(question: question, answers: anlist, coanswer: coanswer);
+}
